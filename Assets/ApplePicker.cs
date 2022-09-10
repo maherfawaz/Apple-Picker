a917d5d666a38a15707f2ffd67bec9178c291a7c
maherfawaz <maherfawaz@outlook.com>
2022-09-10 19:57:06 -0400

    Progress

--- Assets/ApplePicker.cs
+++ Assets/ApplePicker.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ApplePicker : MonoBehaviour {
     [Header("Inscribed")]
@@ -8,19 +9,31 @@ public class ApplePicker : MonoBehaviour {
     public int numBaskets = 3;
     public float basketBottomY = -14f;
     public float basketSpacingY = 2f;
+    public List<GameObject> basketList;
 
     void Start() {
+        basketList = new List<GameObject>();
         for (int i = 0; i < numBaskets; i++) {
             GameObject tBasketGO = Instantiate<GameObject>(basketPrefab);
             Vector3 pos = Vector3.zero;
             pos.y = basketBottomY + (basketSpacingY * i);
             tBasketGO.transform.position = pos;
+            basketList.Add(tBasketGO);
         }
     }
     public void AppleMissed() {
         GameObject[] appleArray = GameObject.FindGameObjectsWithTag("Apple");
         foreach (GameObject tempGO in appleArray) {
             Destroy(tempGO);
+        }
+
+        int basketIndex = basketList.Count -1;
+        GameObject basketGO = basketList[basketIndex];
+        basketList.RemoveAt(basketIndex);
+        Destroy(basketGO);
+
+        if (basketList.Count == 0) {
+            SceneManager.LoadScene("_Scene_0");
         }
     }
 }
