7c7f74685c22dfba6a214c3208239919d518b00e
maherfawaz <maherfawaz@outlook.com>
2022-09-10 20:17:41 -0400

    Update HighScore.cs

--- Assets/HighScore.cs
+++ Assets/HighScore.cs
@@ -11,12 +11,18 @@ public class HighScore : MonoBehaviour {
 
     void Awake() {
         _UI_TEXT = GetComponent<Text>();
+
+        if(PlayerPrefs.HasKey("High Score")) {
+            SCORE = PlayerPrefs.GetInt("HighScore");
+        }
+        PlayerPrefs.SetInt("HighScore", SCORE);
     }
 
     static public int SCORE {
         get { return _SCORE; }
         private set {
             _SCORE = value;
+            PlayerPrefs.SetInt("HighScore", value);
             if (_UI_TEXT != null) {
                 _UI_TEXT.text = "High Score" + value.ToString("#,0");
             }
@@ -27,4 +33,15 @@ public class HighScore : MonoBehaviour {
         if (scoreToTry <= SCORE) return;
         SCORE = scoreToTry;
         }
+
+    [Tooltip("Check this box to reset the HighScore in PlayerPrefs")]
+    public bool resetHighScoreNow = false;
+
+    void OnDrawGizmos() {
+        if(resetHighScoreNow) {
+            resetHighScoreNow = false;
+            PlayerPrefs.SetInt("HighScore", 1000);
+            Debug.LogWarning("PlayerPerfs HighScore reset to 1,000.");
+        }
+    }
 }
